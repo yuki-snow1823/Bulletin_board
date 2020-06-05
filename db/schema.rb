2a2bce74fab238ccba901bb7cea8bdf7750fe12d
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-05 17:21:31 +0900

    [add]Commentモデルの作成、マイグレーション(全てのモデルの完成)

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_081052) do
+ActiveRecord::Schema.define(version: 2020_06_05_082014) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.text "context"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
