7365b4fac442d68979c7eb6680574f9b8c5fa96e
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-06 06:56:41 +0900

    [add]新規投稿機能、ルーティング完成　#2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,25 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.new
   end
 
   def show
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to @post, notice: "successfully created post!"
+    else
+      @posts = Post.all
+      render action: :index
+    end
+  end
+
+  private
+
+  def post_params
+  	params.require(:post).permit(:title, :context)
   end
 end
