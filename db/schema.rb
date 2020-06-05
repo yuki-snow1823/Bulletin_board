9d3d392bfe59d3b00cbf55194d1d3dca5ca89618
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-05 16:46:00 +0900

    [add]Postモデルの作成とマイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_060347) do
+ActiveRecord::Schema.define(version: 2020_06_05_074500) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "context"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
