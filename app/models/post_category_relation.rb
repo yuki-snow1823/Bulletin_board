dae9e944d085d703953fd2fa1a4f5061c2412502
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-05 16:49:43 +0900

    [add]PostとCategoryの中間テーブル作成、マイグレーション

---
+++ app/models/post_category_relation.rb
@@ -0,0 +1,2 @@
+class PostCategoryRelation < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_074500) do
+ActiveRecord::Schema.define(version: 2020_06_05_074749) do
+
+  create_table "post_category_relations", force: :cascade do |t|
+    t.string "post_id"
+    t.string "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
