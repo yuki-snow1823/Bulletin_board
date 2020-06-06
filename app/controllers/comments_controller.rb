7eea4f2dea74acb518e67a1cad2f0c6113b04017
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-06 20:39:08 +0900

    [add]レス投稿機能、レス一覧閲覧機能追加 #3

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(post_comment_params)
+    @comment.user_id = current_user.id
+    @comment.post_id = @post.id
+    @comment.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+  def post_comment_params
+    params.require(:comment).permit(:context)
+  end
+
 end
