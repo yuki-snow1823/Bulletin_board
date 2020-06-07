978ed8734aa4e87205205f14ee9db0ae48175601
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-07 09:49:10 +0900

    [fix]Postへの投稿の際に、禁止ワードが含まれていた場合変な日本語訳になる部分の修正 #17

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,10 +6,10 @@ class Post < ApplicationRecord
 
   validates :title, presence: true
   validates :title, length: { maximum: 30 } 
-  validates :title, exclusion: { in: %w(死ね 殺す) }
+  validates :title, exclusion: { in: %w( 死ね 殺す ) , message: 'に禁止ワードが含まれています' }
   validates :context, presence: true
   validates :context, length: { maximum: 400 }
-  validates :context, exclusion: { in: %w(死ね 殺す ) }
+  validates :context, exclusion: { in: %w( 死ね 殺す ) , message: 'に禁止ワードが含まれています' }
 
   # あとでまとめる
   # with_options presence: true do
