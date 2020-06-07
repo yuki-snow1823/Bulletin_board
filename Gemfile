50c7160a2d4992bc2f6be0bc83f4e1e75312b831
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-07 10:56:37 +0900

    [add]Bootstrap4の導入 #28

--- Gemfile
+++ Gemfile
@@ -56,3 +56,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'ransack'
 gem 'rails-i18n'
+gem 'bootstrap', '~> 4.1.1'
+gem 'jquery-rails'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -15,3 +16,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
