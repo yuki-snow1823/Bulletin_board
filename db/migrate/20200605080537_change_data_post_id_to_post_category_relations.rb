3f75e298f29a7df7e4773f602c3365aba0fa037f
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-09 15:59:25 +0900

    [add]マイグレーションファイルの修正

--- db/migrate/20200605080537_change_data_post_id_to_post_category_relations.rb
+++ db/migrate/20200605080537_change_data_post_id_to_post_category_relations.rb
@@ -1,5 +1,5 @@
 class ChangeDataPostIdToPostCategoryRelations < ActiveRecord::Migration[6.0]
   def change
-      change_column :post_category_relations, :post_id, 'post_id USING CAST(hoge AS integer)'
+      change_column :post_category_relations, :post_id, 'integer USING CAST(post_id AS integer)'
   end
 end
