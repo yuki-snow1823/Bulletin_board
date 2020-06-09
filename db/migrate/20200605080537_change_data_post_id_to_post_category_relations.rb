b2c1417db0047f1a4a83a2d551d6cd24ea6438de
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-09 15:55:25 +0900

    [add]ポスグレがstringからintegerに変形できないため、マイグレーションの修正

--- db/migrate/20200605080537_change_data_post_id_to_post_category_relations.rb
+++ db/migrate/20200605080537_change_data_post_id_to_post_category_relations.rb
@@ -1,6 +1,5 @@
 class ChangeDataPostIdToPostCategoryRelations < ActiveRecord::Migration[6.0]
   def change
-      change_column :post_category_relations, :post_id, :integer
+      change_column :post_category_relations, :post_id, 'post_id USING CAST(hoge AS integer)'
   end
 end
-
--- db/migrate/20200605081052_change_data_category_id_to_post_category_relations.rb
+++ db/migrate/20200605081052_change_data_category_id_to_post_category_relations.rb
@@ -1,5 +1,5 @@
 class ChangeDataCategoryIdToPostCategoryRelations < ActiveRecord::Migration[6.0]
   def change
-      change_column :post_category_relations, :category_id, :integer
+      change_column :post_category_relations, :category_id, 'integer USING CAST(category_id AS integer)'
   end
 end
